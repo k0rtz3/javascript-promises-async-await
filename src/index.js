c1b5db998e5e898aa729ef5b1ec6862931cbf394
k0rtz3 <k0rtz3@users.noreply.github.com>
2023-05-01 19:37:35 +0200

    module 4: Tests running correctly locally

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import { fetchWithTimeout } from "./services";
+import { fetchMovies, fetchBooks } from "./services";
+
 const movies = require("./data/movies.json");
-export function fetchMovies() {
-  const resolveFunction = () => movies;
-  return fetchWithTimeout(1000).then(resolveFunction);
+
+const getBooksAndMovies = () => {
+  return Promise.all([fetchBooks(), fetchMovies()])
+    .then(([books, movies]) => ({
+      books,
+      movies,
+    }))
+    .catch((error) => console.log("Error fetching books and movies" + error));
+};
+
+const getBooksAndMoviesPromise = getBooksAndMovies();
+
+getBooksAndMoviesPromise.then((results) => {
+  console.log("getBooksAndMoviesPromise", results);
+});
+
+function getBooksOrMovies() {
+  return Promise.race([fetchBooks(), fetchMovies()])
+    .then((results) => results)
+    .catch((error) => console.log("Error waiting for the promise", error));
 }
 
-const moviesPromise = fetchMovies();
-moviesPromise.then(results => {
-    console.log(results);
+const getBooksOrMoviesPromise = getBooksOrMovies();
+
+getBooksOrMoviesPromise.then(results => {
+    console.log("getBooksOrMoviesPromise", results)
 })
-
-
--- src/services.js
+++ src/services.js
@@ -1,3 +1,13 @@
-export function fetchWithTimeout(delay) {
-    return new Promise(resolve => setTimeout(resolve, delay))
+export function fetchMovies() {
+  return fetch("./data/movies.json")
+    .then((response) => response.json())
+    .then((movies) => movies)
+    .catch((error) => console.error(`Error: ${error}`));
+}
+
+export function fetchBooks() {
+  return fetch("./data/books.json")
+    .then((response) => response.json())
+    .then((books) => books)
+    .catch((error) => console.error(error));
 }
